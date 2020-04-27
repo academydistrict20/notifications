a6edb2846a74386c2e7a97709026eb31eb3841d6
Luke Harris <luke.harris@lukes-mac.lan>
2020-04-27 11:26:14 -0600

    basic build serve web component

---
+++ web-components/dist/my-component.3.js
@@ -0,0 +1,43 @@
+(window["myComponent_jsonp"] = window["myComponent_jsonp"] || []).push([[3],{
+
+/***/ "2476":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"2291231a-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/atoms/icons/ChevronSvg.vue?vue&type=template&id=5b83af90&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('g',[_c('path',{staticClass:"line",attrs:{"fill":"currentColor","d":"M16 11.05l-8.49 8.49 1.42 1.41L16 13.88l7.07 7.07 1.41-1.41L16 11.05z"}})])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./src/components/atoms/icons/ChevronSvg.vue?vue&type=template&id=5b83af90&
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__("2877");
+
+// CONCATENATED MODULE: ./src/components/atoms/icons/ChevronSvg.vue
+
+var script = {}
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  script,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  null
+  ,true
+)
+
+/* harmony default export */ var ChevronSvg = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=my-component.3.js.map
