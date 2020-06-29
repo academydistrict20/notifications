7821e75602b47008e5c1963156c0b72e41d0c9f8
Lukeharris30 <lukeharris.med@gmail.com>
2020-06-29 09:20:30 -0600

    Lukeharris30/issue107 (#108)
    * fix: begin web component embed
    
    * fix(embed): fix styles and prop / attribute passing
    
    Co-authored-by: Luke Harris <luke.haris@asd20.org>

--- packages/embed/src/components/Asd20NotificationsEmbed.vue
+++ packages/embed/src/components/Asd20NotificationsEmbed.vue
@@ -1,36 +1,62 @@
-<template>
-  <div :id="`asd20-notifications-embed`" v-if="nextTick">
-    <mounting-portal
-      v-for="(group, type) of groups"
-      :key="type"
-      :mountTo="groupTargetSelectors[type]"
-      append
-    >
-      <Asd20NotificationGroup 
-        :notifications="activeNotificationsByType[type]"
-        :type="type"
-        @dismiss="onDismiss"
-        :position="group.position"
-      ></Asd20NotificationGroup>
-    </mounting-portal>
-  </div>
-</template>
-
 <script>
 // Client
 import NotificationClient from '@asd20/notifications-client'
 
 // Components
 import { MountingPortal } from 'portal-vue'
-import Asd20NotificationGroup from '@asd20/notifications-ui/src/components/Asd20NotificationGroup.vue'
-// import Asd20NotificationGroup from '../../../ui/src/components/Asd20NotificationGroup.vue'
 
 export default {
+  render(h) {
+    const mountingPortalNodes = []
+
+    for (const [type, group] of Object.entries(this.groups)) {
+      console.log('group', group, 'type', type)
+      mountingPortalNodes.push(h(
+        'mounting-portal',
+        {
+          props: {
+            key: type,
+            mountTo: this.groupTargetSelectors[type],
+            append: true
+          }
+        },
+        [
+          h(
+            'asd20-notification-group',
+            {
+              domProps: {
+                notifications: this.activeNotificationsByType[type],
+              },
+              attrs: {
+                grouptype: type, 
+                position: group.position
+              },
+              on: {
+                dismiss: this.onDismiss
+              }
+            }
+          )
+        ]
+      ))
+    }
+
+    if (this.nextTick) {
+      return h(
+        'div', // Tag name
+        {      // Options
+          attrs: {
+            id: 'asd20-notifications-embed'
+          }
+        },
+        mountingPortalNodes
+      )
+    }
+  },
+
   name: 'asd20-notifications-embed',
 
   components: {
-    MountingPortal,
-    Asd20NotificationGroup,
+    MountingPortal
   },
 
   props: {
@@ -126,7 +152,9 @@ export default {
 
   methods: {
     // Asks the client to dismiss a notification
-    onDismiss(notification) {
+    onDismiss(event) {
+      const notification = event.detail[0]
+      console.log('dismissed', notification)
       if (!this.client) return
       this.client.dismiss(notification)
     },
